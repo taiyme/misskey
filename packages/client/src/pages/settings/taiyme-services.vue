6a519418df3c25416fef22fa1ff5349ad0dd680b
taiyme <53635909+taiyme@users.noreply.github.com>
2023-02-17 04:21:31 +0900

    enhance(client): tms: タブ機能強化

--- packages/client/src/pages/settings/taiyme-services.vue
+++ packages/client/src/pages/settings/taiyme-services.vue
@@ -1,79 +1,79 @@
 <template>
 <MkStickyContainer>
 	<template #header><XHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
-	<div class="_formRoot">
-		<template v-if="tab === 'overview'">
-			<FormSection>
-				<MkKeyValue class="_formBlock" :copy="version">
-					<template #key>{{ i18n.ts.version }}</template>
-					<template #value>{{ version }}</template>
-				</MkKeyValue>
-			</FormSection>
+	<MkSpacer :content-max="900">
+		<div class="_formRoot">
+			<template v-if="tab === 'overview'">
+				<FormSection>
+					<MkKeyValue class="_formBlock" :copy="version">
+						<template #key>{{ i18n.ts.version }}</template>
+						<template #value>{{ version }}</template>
+					</MkKeyValue>
+				</FormSection>
 
-			<FormSection>
-				<MkKeyValue class="_formBlock">
-					<template #key>taiyme/misskeyについて</template>
-					<template #value>taiyme/misskeyは、taiyによって無償でメンテナンスされています。<br>継続して提供するためにも、開発のサポートや寄付をお願いします。</template>
-				</MkKeyValue>
-				<div class="_formLinks">
-					<FormLink to="https://github.com/taiyme/misskey" external>
-						<template #icon><i class="ti ti-code"></i></template>
-						taiyme/misskeyの開発に参加
-						<template #suffix>GitHub</template>
-					</FormLink>
-					<FormLink to="https://u.taiy.me/donate" external>
-						<template #icon><i class="ti ti-pig-money"></i></template>
-						taiyに寄付
-						<template #suffix>Donate</template>
-					</FormLink>
-				</div>
-			</FormSection>
-		</template>
+				<FormSection>
+					<MkKeyValue class="_formBlock">
+						<template #key>taiyme/misskeyについて</template>
+						<template #value>taiyme/misskeyは、taiyによって無償でメンテナンスされています。<br>継続して提供するためにも、開発のサポートや寄付をお願いします。</template>
+					</MkKeyValue>
+					<div class="_formLinks">
+						<FormLink to="https://github.com/taiyme/misskey" external>
+							<template #icon><i class="ti ti-code"></i></template>
+							taiyme/misskeyの開発に参加
+							<template #suffix>GitHub</template>
+						</FormLink>
+						<FormLink to="https://u.taiy.me/donate" external>
+							<template #icon><i class="ti ti-pig-money"></i></template>
+							taiyに寄付
+							<template #suffix>Donate</template>
+						</FormLink>
+					</div>
+				</FormSection>
+			</template>
 
-		<template v-else-if="tab === 'settings'">
-			<FormSection>
-				<MkInfo warn class="_formBlock">設定はページリロード後に反映されます。</MkInfo>
+			<template v-else-if="tab === 'settings'">
+				<FormSection>
+					<FormSwitch v-model="tmsVerticalInstanceTicker" class="_formBlock">
+						ノートのインスタンス情報を左端に表示
+						<template #caption>タイムライン上のインスタンス情報を左端に表示します。</template>
+					</FormSwitch>
 
-				<FormSwitch v-model="tmsVerticalInstanceTicker" class="_formBlock">
-					ノートのインスタンス情報を左端に表示
-					<template #caption>タイムライン上のインスタンス情報を左端に表示します。</template>
-				</FormSwitch>
+					<FormSwitch v-model="tmsIsLongEnabled" class="_formBlock">
+						ノートを畳む
+						<template #caption>タイムライン上のノートが特定の条件の場合、畳んで表示します。無効にすると常に展開して表示します。</template>
+					</FormSwitch>
+					<FormFolder>
+						<template #label>ノートを畳む条件</template>
+						<template v-if="tmsIsLongEnabled" #suffix>有効</template>
+						<template v-else #suffix>無効</template>
 
-				<FormSwitch v-model="tmsIsLongEnabled" class="_formBlock">
-					ノートを畳む
-					<template #caption>タイムライン上のノートが特定の条件の場合、畳んで表示します。無効にすると常に展開して表示します。</template>
-				</FormSwitch>
-				<FormFolder>
-					<template #label>ノートを畳む条件</template>
-					<template v-if="tmsIsLongEnabled" #suffix>有効</template>
-					<template v-else #suffix>無効</template>
+						<MkInfo class="_formBlock">0以上の整数を指定してください。0を指定すると条件が無効になります。</MkInfo>
 
-					<MkInfo class="_formBlock">0以上の整数を指定してください。0を指定すると条件が無効になります。</MkInfo>
-
-					<FormInput v-model="tmsIsLongTextElHeight" type="number" class="_formBlock">
-						<template #label>ノート本文の高さ</template>
-						<template #suffix>px</template>
-						<template #caption>タイムライン上のノートがこの高さを超えた場合、畳んで表示します。ピクセル単位で指定してください。</template>
-					</FormInput>
-					<FormInput v-model="tmsIsLongFilesLength" type="number" class="_formBlock">
-						<template #label>添付ファイルの個数</template>
-						<template #suffix>個</template>
-						<template #caption>タイムライン上のノートの添付ファイルがこの個数を超えた場合、畳んで表示します。</template>
-					</FormInput>
-					<FormInput v-model="tmsIsLongUrlsLength" type="number" class="_formBlock">
-						<template #label>URLの個数</template>
-						<template #suffix>個</template>
-						<template #caption>タイムライン上のノートのURLがこの個数を超えた場合、畳んで表示します。</template>
-					</FormInput>
-					<FormInput v-model="tmsIsLongPollLength" type="number" class="_formBlock">
-						<template #label>アンケートの選択肢の個数</template>
-						<template #suffix>個</template>
-						<template #caption>タイムライン上のノートのアンケートの選択肢がこの個数を超えた場合、畳んで表示します。</template>
-					</FormInput>
-				</FormFolder>
-			</FormSection>
-		</template>
-	</div>
+						<FormInput v-model="tmsIsLongTextElHeight" type="number" class="_formBlock">
+							<template #label>ノート本文の高さ</template>
+							<template #suffix>px</template>
+							<template #caption>タイムライン上のノートがこの高さを超えた場合、畳んで表示します。ピクセル単位で指定してください。</template>
+						</FormInput>
+						<FormInput v-model="tmsIsLongFilesLength" type="number" class="_formBlock">
+							<template #label>添付ファイルの個数</template>
+							<template #suffix>個</template>
+							<template #caption>タイムライン上のノートの添付ファイルがこの個数を超えた場合、畳んで表示します。</template>
+						</FormInput>
+						<FormInput v-model="tmsIsLongUrlsLength" type="number" class="_formBlock">
+							<template #label>URLの個数</template>
+							<template #suffix>個</template>
+							<template #caption>タイムライン上のノートのURLがこの個数を超えた場合、畳んで表示します。</template>
+						</FormInput>
+						<FormInput v-model="tmsIsLongPollLength" type="number" class="_formBlock">
+							<template #label>アンケートの選択肢の個数</template>
+							<template #suffix>個</template>
+							<template #caption>タイムライン上のノートのアンケートの選択肢がこの個数を超えた場合、畳んで表示します。</template>
+						</FormInput>
+					</FormFolder>
+				</FormSection>
+			</template>
+		</div>
+	</MkSpacer>
 </MkStickyContainer>
 </template>
 
@@ -92,6 +92,7 @@ import { unisonReload } from '@/scripts/unison-reload';
 import { i18n } from '@/i18n';
 import { version } from '@/config';
 import { defaultStore } from '@/store';
+import { definePageMetadata } from '@/scripts/page-metadata';
 
 let tab = $ref('overview');
 
@@ -106,6 +107,7 @@ const headerTabs = $computed(() => [{
 }]);
 
 const headerActions = $computed(() => [{
+	asFullButton: true,
 	icon: 'ti ti-check',
 	text: i18n.ts.save,
 	handler: save,
@@ -157,4 +159,9 @@ async function save() {
 		});
 	}
 }
+
+definePageMetadata({
+	title: 'taiyme-services',
+	icon: 'ti ti-square-plus',
+});
 </script>
