ee26dbeb8ea49e6355ec1202ef320036e699847c
taiyme <53635909+taiyme@users.noreply.github.com>
2023-02-17 05:22:03 +0900

    enhance(client): tms: MkTab

--- packages/client/src/pages/settings/taiyme-services.vue
+++ packages/client/src/pages/settings/taiyme-services.vue
@@ -1,92 +1,94 @@
 <template>
-<MkStickyContainer>
-	<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
-	<MkSpacer :content-max="900">
-		<div class="_contents">
-			<div v-if="tab === 'overview'" class="_formRoot">
-				<FormSection>
-					<MkKeyValue class="_formBlock" :copy="version">
-						<template #key>{{ i18n.ts.version }}</template>
-						<template #value>{{ version }}</template>
-					</MkKeyValue>
-				</FormSection>
+<MkTab v-model="tab" style="margin-bottom: var(--margin);">
+	<option value="overview">{{ i18n.ts.overview }}</option>
+	<option value="settings">{{ i18n.ts.settings }}</option>
+</MkTab>
 
-				<FormSection>
-					<MkKeyValue class="_formBlock">
-						<template #key>taiyme/misskeyについて</template>
-						<template #value>taiyme/misskeyは、taiyによって無償でメンテナンスされています。<br>継続して提供するためにも、開発のサポートや寄付をお願いします。</template>
-					</MkKeyValue>
-					<div class="_formLinks">
-						<FormLink to="https://github.com/taiyme/misskey" external>
-							<template #icon><i class="ti ti-code"></i></template>
-							taiyme/misskeyの開発に参加
-							<template #suffix>GitHub</template>
-						</FormLink>
-						<FormLink to="https://u.taiy.me/donate" external>
-							<template #icon><i class="ti ti-pig-money"></i></template>
-							taiyに寄付
-							<template #suffix>Donate</template>
-						</FormLink>
-					</div>
-				</FormSection>
-			</div>
+<div v-if="tab === 'overview'" class="_formRoot">
+	<FormSection>
+		<MkKeyValue class="_formBlock" :copy="version">
+			<template #key>{{ i18n.ts.version }}</template>
+			<template #value>{{ version }}</template>
+		</MkKeyValue>
+	</FormSection>
 
-			<div v-else-if="tab === 'settings'" class="_formRoot">
-				<FormSection>
-					<FormSwitch v-model="tmsVerticalInstanceTicker" class="_formBlock">
-						ノートのインスタンス情報を左端に表示
-						<template #caption>タイムライン上のインスタンス情報を左端に表示します。</template>
-					</FormSwitch>
-
-					<FormSwitch v-model="tmsIsLongEnabled" class="_formBlock">
-						ノートを畳む
-						<template #caption>タイムライン上のノートが特定の条件の場合、畳んで表示します。無効にすると常に展開して表示します。</template>
-					</FormSwitch>
-					<FormFolder>
-						<template #label>ノートを畳む条件</template>
-						<template v-if="tmsIsLongEnabled" #suffix>有効</template>
-						<template v-else #suffix>無効</template>
-
-						<div class="_formRoot">
-							<MkInfo class="_formBlock">0以上の整数を指定してください。0を指定すると条件が無効になります。</MkInfo>
-							<FormInput v-model="tmsIsLongTextElHeight" type="number" class="_formBlock">
-								<template #label>ノート本文の高さ</template>
-								<template #suffix>px</template>
-								<template #caption>タイムライン上のノートがこの高さを超えた場合、畳んで表示します。ピクセル単位で指定してください。</template>
-							</FormInput>
-							<FormInput v-model="tmsIsLongFilesLength" type="number" class="_formBlock">
-								<template #label>添付ファイルの個数</template>
-								<template #suffix>個</template>
-								<template #caption>タイムライン上のノートの添付ファイルがこの個数を超えた場合、畳んで表示します。</template>
-							</FormInput>
-							<FormInput v-model="tmsIsLongUrlsLength" type="number" class="_formBlock">
-								<template #label>URLの個数</template>
-								<template #suffix>個</template>
-								<template #caption>タイムライン上のノートのURLがこの個数を超えた場合、畳んで表示します。</template>
-							</FormInput>
-							<FormInput v-model="tmsIsLongPollLength" type="number" class="_formBlock">
-								<template #label>アンケートの選択肢の個数</template>
-								<template #suffix>個</template>
-								<template #caption>タイムライン上のノートのアンケートの選択肢がこの個数を超えた場合、畳んで表示します。</template>
-							</FormInput>
-						</div>
-					</FormFolder>
-				</FormSection>
-			</div>
+	<FormSection>
+		<MkKeyValue class="_formBlock">
+			<template #key>taiyme/misskeyについて</template>
+			<template #value>taiyme/misskeyは、taiyによって無償でメンテナンスされています。<br>継続して提供するためにも、開発のサポートや寄付をお願いします。</template>
+		</MkKeyValue>
+		<div class="_formLinks">
+			<FormLink to="https://github.com/taiyme/misskey" external>
+				<template #icon><i class="ti ti-code"></i></template>
+				taiyme/misskeyの開発に参加
+				<template #suffix>GitHub</template>
+			</FormLink>
+			<FormLink to="https://u.taiy.me/donate" external>
+				<template #icon><i class="ti ti-pig-money"></i></template>
+				taiyに寄付
+				<template #suffix>Donate</template>
+			</FormLink>
 		</div>
-	</MkSpacer>
-</MkStickyContainer>
+	</FormSection>
+</div>
+
+<div v-else-if="tab === 'settings'" class="_formRoot">
+	<MkButton class="_formBlock" primary :disabled="!changed" @click="save"><i class="ti ti-device-floppy"></i> {{ i18n.ts.save }}</MkButton>
+
+	<FormSection>
+		<FormSwitch v-model="tmsVerticalInstanceTicker" class="_formBlock">
+			ノートのインスタンス情報を左端に表示
+			<template #caption>タイムライン上のインスタンス情報を左端に表示します。</template>
+		</FormSwitch>
+
+		<FormSwitch v-model="tmsIsLongEnabled" class="_formBlock">
+			ノートを畳む
+			<template #caption>タイムライン上のノートが特定の条件の場合、畳んで表示します。無効にすると常に展開して表示します。</template>
+		</FormSwitch>
+		<FormFolder>
+			<template #label>ノートを畳む条件</template>
+			<template v-if="tmsIsLongEnabled" #suffix>有効</template>
+			<template v-else #suffix>無効</template>
+
+			<div class="_formRoot">
+				<MkInfo class="_formBlock">0以上の整数を指定してください。0を指定すると条件が無効になります。</MkInfo>
+				<FormInput v-model="tmsIsLongTextElHeight" type="number" class="_formBlock">
+					<template #label>ノート本文の高さ</template>
+					<template #suffix>px</template>
+					<template #caption>タイムライン上のノートがこの高さを超えた場合、畳んで表示します。ピクセル単位で指定してください。</template>
+				</FormInput>
+				<FormInput v-model="tmsIsLongFilesLength" type="number" class="_formBlock">
+					<template #label>添付ファイルの個数</template>
+					<template #suffix>個</template>
+					<template #caption>タイムライン上のノートの添付ファイルがこの個数を超えた場合、畳んで表示します。</template>
+				</FormInput>
+				<FormInput v-model="tmsIsLongUrlsLength" type="number" class="_formBlock">
+					<template #label>URLの個数</template>
+					<template #suffix>個</template>
+					<template #caption>タイムライン上のノートのURLがこの個数を超えた場合、畳んで表示します。</template>
+				</FormInput>
+				<FormInput v-model="tmsIsLongPollLength" type="number" class="_formBlock">
+					<template #label>アンケートの選択肢の個数</template>
+					<template #suffix>個</template>
+					<template #caption>タイムライン上のノートのアンケートの選択肢がこの個数を超えた場合、畳んで表示します。</template>
+				</FormInput>
+			</div>
+		</FormFolder>
+	</FormSection>
+</div>
 </template>
 
 <script lang="ts" setup>
-import { } from 'vue';
+import { watch } from 'vue';
 import FormLink from '@/components/form/link.vue';
 import FormSwitch from '@/components/form/switch.vue';
 import FormInput from '@/components/form/input.vue';
 import FormSection from '@/components/form/section.vue';
 import FormFolder from '@/components/form/folder.vue';
+import MkButton from '@/components/MkButton.vue';
 import MkKeyValue from '@/components/MkKeyValue.vue';
 import MkInfo from '@/components/MkInfo.vue';
+import MkTab from '@/components/MkTab.vue';
 import * as os from '@/os';
 import { unisonReload } from '@/scripts/unison-reload';
 import { i18n } from '@/i18n';
@@ -94,23 +96,7 @@ import { version } from '@/config';
 import { defaultStore } from '@/store';
 
 let tab = $ref('overview');
-
-const headerTabs = $computed(() => [{
-	key: 'overview',
-	title: i18n.ts.overview,
-	icon: 'ti ti-info-circle',
-}, {
-	key: 'settings',
-	title: i18n.ts.settings,
-	icon: 'ti ti-settings',
-}]);
-
-const headerActions = $computed(() => [{
-	asFullButton: true,
-	icon: 'ti ti-check',
-	text: i18n.ts.save,
-	handler: save,
-}]);
+let changed = $ref(false);
 
 const tmsVerticalInstanceTicker = $ref(defaultStore.state.tmsVerticalInstanceTicker);
 const tmsIsLongEnabled = $ref(defaultStore.state.tmsIsLongEnabled);
@@ -118,6 +104,17 @@ const tmsIsLongTextElHeight = $ref(defaultStore.state.tmsIsLongTextElHeight);
 const tmsIsLongFilesLength = $ref(defaultStore.state.tmsIsLongFilesLength);
 const tmsIsLongUrlsLength = $ref(defaultStore.state.tmsIsLongUrlsLength);
 const tmsIsLongPollLength = $ref(defaultStore.state.tmsIsLongPollLength);
+
+watch([
+	$$(tmsVerticalInstanceTicker),
+	$$(tmsIsLongEnabled),
+	$$(tmsIsLongTextElHeight),
+	$$(tmsIsLongFilesLength),
+	$$(tmsIsLongUrlsLength),
+	$$(tmsIsLongPollLength),
+], () => {
+	changed = true;
+});
 
 async function check() {
 	const isNumberInRange = (x: number, min?: number, max?: number): boolean => {
