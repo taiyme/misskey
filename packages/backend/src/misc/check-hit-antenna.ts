031f35cc5550d0272f69dacd2d4c062b8569f811
Steve-0628 <github@hmpf.club>
2023-02-20 21:41:50 +0900

    antennas should work with followers and direct notes

--- packages/backend/src/misc/check-hit-antenna.ts
+++ packages/backend/src/misc/check-hit-antenna.ts
@@ -1,7 +1,7 @@
 import { Antenna } from '@/models/entities/antenna.js';
 import { Note } from '@/models/entities/note.js';
 import { User } from '@/models/entities/user.js';
-import { UserListJoinings, UserGroupJoinings, Blockings } from '@/models/index.js';
+import { UserListJoinings, UserGroupJoinings, Blockings, Notes } from '@/models/index.js';
 import { getFullApAccount } from './convert-host.js';
 import * as Acct from '@/misc/acct.js';
 import { Packed } from './schema.js';
@@ -12,39 +12,10 @@ const blockingCache = new Cache<User['id'][]>(1000 * 60 * 5);
 // NOTE: フォローしているユーザーのノート、リストのユーザーのノート、グループのユーザーのノート指定はパフォーマンス上の理由で無効になっている
 
 export async function checkHitAntenna(antenna: Antenna, note: (Note | Packed<'Note'>), noteUser: { id: User['id']; username: string; host: string | null; }): Promise<boolean> {
-	if (note.visibility === 'specified') return false;
-	if (note.visibility === 'followers') return false;
-
-	// アンテナ作成者がノート作成者にブロックされていたらスキップ
-	const blockings = await blockingCache.fetch(noteUser.id, () => Blockings.findBy({ blockerId: noteUser.id }).then(res => res.map(x => x.blockeeId)));
-	if (blockings.some(blocking => blocking === antenna.userId)) return false;
-
-	if (!antenna.withReplies && note.replyId != null) return false;
-
-	if (antenna.src === 'home') {
-		// TODO
-	} else if (antenna.src === 'list') {
-		const listUsers = (await UserListJoinings.findBy({
-			userListId: antenna.userListId!,
-		})).map(x => x.userId);
-
-		if (!listUsers.includes(note.userId)) return false;
-	} else if (antenna.src === 'group') {
-		const joining = await UserGroupJoinings.findOneByOrFail({ id: antenna.userGroupJoiningId! });
-
-		const groupUsers = (await UserGroupJoinings.findBy({
-			userGroupId: joining.userGroupId,
-		})).map(x => x.userId);
-
-		if (!groupUsers.includes(note.userId)) return false;
-	} else if (antenna.src === 'users') {
-		const accts = antenna.users.map(x => {
-			const { username, host } = Acct.parse(x);
-			return getFullApAccount(username, host).toLowerCase();
-		});
-		if (!accts.includes(getFullApAccount(noteUser.username, noteUser.host).toLowerCase())) return false;
+	if ( !(await Notes.isVisibleForMe(note, antenna.userId))) {
+		return false;
 	}
-
+	
 	const keywords = antenna.keywords
 		// Clean up
 		.map(xs => xs.filter(x => x !== ''))
