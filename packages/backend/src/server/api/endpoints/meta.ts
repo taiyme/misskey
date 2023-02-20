2b2d06c35db31b1a0ed7fcff5cf4419c89a931e3
taiyme <53635909+taiyme@users.noreply.github.com>
2023-02-21 01:25:13 +0900

    change repo

--- packages/backend/src/server/api/endpoints/meta.ts
+++ packages/backend/src/server/api/endpoints/meta.ts
@@ -57,12 +57,12 @@ export const meta = {
 			repositoryUrl: {
 				type: 'string',
 				optional: false, nullable: false,
-				default: 'https://github.com/misskey-dev/misskey',
+				default: 'https://github.com/taiyme/misskey',
 			},
 			feedbackUrl: {
 				type: 'string',
 				optional: false, nullable: false,
-				default: 'https://github.com/misskey-dev/misskey/issues/new',
+				default: 'https://github.com/taiyme/misskey/issues/new',
 			},
 			defaultDarkTheme: {
 				type: 'string',
