81bc398e12b9065716c164c8ff4a2bbb150a1372
taiy <53635909+taiyme@users.noreply.github.com>
2023-03-13 05:14:35 +0900

    change: tms: 変数名を変更

--- packages/client/src/pages/settings/taiyme-services.vue
+++ packages/client/src/pages/settings/taiyme-services.vue
@@ -75,33 +75,33 @@
 	</FormSection>
 
 	<FormSection>
-		<FormSwitch v-model="tmsIsLongEnabled" class="_formBlock">
+		<FormSwitch v-model="tmsCollapseNote" class="_formBlock">
 			ノートを畳む
 			<template #caption>タイムライン上のノートが特定の条件の場合、畳んで表示します。無効にすると常に展開して表示します。</template>
 		</FormSwitch>
 		<FormFolder>
 			<template #label>ノートを畳む条件</template>
-			<template v-if="tmsIsLongEnabled" #suffix>有効</template>
+			<template v-if="tmsCollapseNote" #suffix>有効</template>
 			<template v-else #suffix>無効</template>
 
 			<div class="_formRoot">
 				<MkInfo class="_formBlock">0以上の整数を指定してください。0を指定すると条件が無効になります。</MkInfo>
-				<FormInput v-model="tmsIsLongTextElHeight" type="number" class="_formBlock">
+				<FormInput v-model="tmsCollapseNoteHeight" type="number" class="_formBlock">
 					<template #label>ノート本文の高さ</template>
 					<template #suffix>px</template>
 					<template #caption>タイムライン上のノートがこの高さを超えた場合、畳んで表示します。ピクセル単位で指定してください。</template>
 				</FormInput>
-				<FormInput v-model="tmsIsLongFilesLength" type="number" class="_formBlock">
+				<FormInput v-model="tmsCollapseNoteFile" type="number" class="_formBlock">
 					<template #label>添付ファイルの個数</template>
 					<template #suffix>個</template>
 					<template #caption>タイムライン上のノートの添付ファイルがこの個数を超えた場合、畳んで表示します。</template>
 				</FormInput>
-				<FormInput v-model="tmsIsLongUrlsLength" type="number" class="_formBlock">
+				<FormInput v-model="tmsCollapseNoteUrl" type="number" class="_formBlock">
 					<template #label>URLの個数</template>
 					<template #suffix>個</template>
 					<template #caption>タイムライン上のノートのURLがこの個数を超えた場合、畳んで表示します。</template>
 				</FormInput>
-				<FormInput v-model="tmsIsLongPollLength" type="number" class="_formBlock">
+				<FormInput v-model="tmsCollapseNotePoll" type="number" class="_formBlock">
 					<template #label>アンケートの選択肢の個数</template>
 					<template #suffix>個</template>
 					<template #caption>タイムライン上のノートのアンケートの選択肢がこの個数を超えた場合、畳んで表示します。</template>
@@ -109,32 +109,32 @@
 			</div>
 		</FormFolder>
 
-		<FormSwitch v-model="tmsRenoteCollapsedEnabled" class="_formBlock">
+		<FormSwitch v-model="tmsCollapseRenote" class="_formBlock">
 			一度見たノートのRenoteを畳む
 			<template #caption>タイムライン上の一度見たノートのRenoteを畳んで表示します。本文をクリックすると展開します。</template>
 		</FormSwitch>
 	</FormSection>
 
 	<FormSection>
-		<FormSwitch v-model="tmsPakuruEnabled" class="_formBlock">
+		<FormSwitch v-model="tmsUsePakuru" class="_formBlock">
 			「パクる」機能を有効にする
 			<template #caption>Renoteメニューに「パクる」を追加します。</template>
 		</FormSwitch>
 
-		<FormSwitch v-model="tmsNumberquoteEnabled" class="_formBlock">
+		<FormSwitch v-model="tmsUseNumberquote" class="_formBlock">
 			「数字引用」機能を有効にする
 			<template #caption>Renoteメニューに「数字引用する」を追加します。</template>
 		</FormSwitch>
 	</FormSection>
 
 	<FormSection>
-		<FormSwitch v-model="tmsImanonashiEnabled" class="_formBlock">
+		<FormSwitch v-model="tmsUseImanonashi" class="_formBlock">
 			「いまのなし」機能を有効にする
 			<template #caption>消せ消せ消せ消せ消せ</template>
 		</FormSwitch>
 		<FormFolder>
 			<template #label>削除する条件</template>
-			<template v-if="tmsImanonashiEnabled" #suffix>有効</template>
+			<template v-if="tmsUseImanonashi" #suffix>有効</template>
 			<template v-else #suffix>無効</template>
 
 			<div class="_formRoot">
@@ -142,11 +142,11 @@
 					<template #label>単語</template>
 					<template #caption>スペースで区切るとAND指定になり、改行で区切るとOR指定になります。<br>キーワードをスラッシュで囲むと正規表現になります。</template>
 				</FormTextarea>
-				<FormSwitch v-model="tmsImanonashiConfirmEnabled" class="_formBlock">
+				<FormSwitch v-model="tmsImanonashiConfirm" class="_formBlock">
 					削除を確認する
 					<template #caption>ノートを削除するときに確認します。</template>
 				</FormSwitch>
-				<FormSwitch v-model="tmsImanonashiDeleteEnabled" class="_formBlock">
+				<FormSwitch v-model="tmsImanonashiItself" class="_formBlock">
 					いまのなしも削除する
 					<template #caption>ノートを削除するときに、同時にいまのなしも削除します。</template>
 				</FormSwitch>
@@ -222,35 +222,35 @@ let changed = $ref(false);
 
 const tmsVerticalInstanceTicker = $ref(tmsStore.state.verticalInstanceTicker);
 const tmsUseReactionMenu = $ref(tmsStore.state.useReactionMenu);
-const tmsIsLongEnabled = $ref(tmsStore.state.isLongEnabled);
-const tmsIsLongTextElHeight = $ref(tmsStore.state.isLongTextElHeight);
-const tmsIsLongFilesLength = $ref(tmsStore.state.isLongFilesLength);
-const tmsIsLongUrlsLength = $ref(tmsStore.state.isLongUrlsLength);
-const tmsIsLongPollLength = $ref(tmsStore.state.isLongPollLength);
-const tmsRenoteCollapsedEnabled = $ref(tmsStore.state.renoteCollapsedEnabled);
-const tmsPakuruEnabled = $ref(tmsStore.state.pakuruEnabled);
-const tmsNumberquoteEnabled = $ref(tmsStore.state.numberquoteEnabled);
-const tmsImanonashiEnabled = $ref(tmsStore.state.imanonashiEnabled);
+const tmsCollapseNote = $ref(tmsStore.state.collapseNote);
+const tmsCollapseNoteHeight = $ref(tmsStore.state.collapseNoteHeight);
+const tmsCollapseNoteFile = $ref(tmsStore.state.collapseNoteFile);
+const tmsCollapseNoteUrl = $ref(tmsStore.state.collapseNoteUrl);
+const tmsCollapseNotePoll = $ref(tmsStore.state.collapseNotePoll);
+const tmsCollapseRenote = $ref(tmsStore.state.collapseRenote);
+const tmsUsePakuru = $ref(tmsStore.state.usePakuru);
+const tmsUseNumberquote = $ref(tmsStore.state.useNumberquote);
+const tmsUseImanonashi = $ref(tmsStore.state.useImanonashi);
 const tmsImanonashiWords = $ref(renderWords(tmsStore.state.imanonashiWords));
-const tmsImanonashiConfirmEnabled = $ref(tmsStore.state.imanonashiConfirmEnabled);
-const tmsImanonashiDeleteEnabled = $ref(tmsStore.state.imanonashiDeleteEnabled);
+const tmsImanonashiConfirm = $ref(tmsStore.state.imanonashiConfirm);
+const tmsImanonashiItself = $ref(tmsStore.state.imanonashiItself);
 
 watch(
 	[
 		$$(tmsVerticalInstanceTicker),
 		$$(tmsUseReactionMenu),
-		$$(tmsIsLongEnabled),
-		$$(tmsIsLongTextElHeight),
-		$$(tmsIsLongFilesLength),
-		$$(tmsIsLongUrlsLength),
-		$$(tmsIsLongPollLength),
-		$$(tmsRenoteCollapsedEnabled),
-		$$(tmsPakuruEnabled),
-		$$(tmsNumberquoteEnabled),
-		$$(tmsImanonashiEnabled),
+		$$(tmsCollapseNote),
+		$$(tmsCollapseNoteHeight),
+		$$(tmsCollapseNoteFile),
+		$$(tmsCollapseNoteUrl),
+		$$(tmsCollapseNotePoll),
+		$$(tmsCollapseRenote),
+		$$(tmsUsePakuru),
+		$$(tmsUseNumberquote),
+		$$(tmsUseImanonashi),
 		$$(tmsImanonashiWords),
-		$$(tmsImanonashiConfirmEnabled),
-		$$(tmsImanonashiDeleteEnabled),
+		$$(tmsImanonashiConfirm),
+		$$(tmsImanonashiItself),
 	],
 	() => {
 		changed = true;
@@ -264,10 +264,10 @@ async function check(): Promise<boolean> {
 		return (min == null ? -Infinity : min) <= x && x <= (max == null ? Infinity : max);
 	};
 	return (
-		isNumberInRange(tmsIsLongTextElHeight, 0) &&
-		isNumberInRange(tmsIsLongFilesLength, 0) &&
-		isNumberInRange(tmsIsLongUrlsLength, 0) &&
-		isNumberInRange(tmsIsLongPollLength, 0) &&
+		isNumberInRange(tmsCollapseNoteHeight, 0) &&
+		isNumberInRange(tmsCollapseNoteFile, 0) &&
+		isNumberInRange(tmsCollapseNoteUrl, 0) &&
+		isNumberInRange(tmsCollapseNotePoll, 0) &&
 		checkWords(tmsImanonashiWords)
 	);
 }
@@ -276,18 +276,18 @@ async function save(): Promise<void> {
 	if (await check()) {
 		tmsStore.set('verticalInstanceTicker', tmsVerticalInstanceTicker);
 		tmsStore.set('useReactionMenu', tmsUseReactionMenu);
-		tmsStore.set('isLongEnabled', tmsIsLongEnabled);
-		tmsStore.set('isLongTextElHeight', tmsIsLongTextElHeight);
-		tmsStore.set('isLongFilesLength', tmsIsLongFilesLength);
-		tmsStore.set('isLongUrlsLength', tmsIsLongUrlsLength);
-		tmsStore.set('isLongPollLength', tmsIsLongPollLength);
-		tmsStore.set('renoteCollapsedEnabled', tmsRenoteCollapsedEnabled);
-		tmsStore.set('pakuruEnabled', tmsPakuruEnabled);
-		tmsStore.set('numberquoteEnabled', tmsNumberquoteEnabled);
-		tmsStore.set('imanonashiEnabled', tmsImanonashiEnabled);
+		tmsStore.set('collapseNote', tmsCollapseNote);
+		tmsStore.set('collapseNoteHeight', tmsCollapseNoteHeight);
+		tmsStore.set('collapseNoteFile', tmsCollapseNoteFile);
+		tmsStore.set('collapseNoteUrl', tmsCollapseNoteUrl);
+		tmsStore.set('collapseNotePoll', tmsCollapseNotePoll);
+		tmsStore.set('collapseRenote', tmsCollapseRenote);
+		tmsStore.set('usePakuru', tmsUsePakuru);
+		tmsStore.set('useNumberquote', tmsUseNumberquote);
+		tmsStore.set('useImanonashi', tmsUseImanonashi);
 		tmsStore.set('imanonashiWords', parseWords(tmsImanonashiWords));
-		tmsStore.set('imanonashiConfirmEnabled', tmsImanonashiConfirmEnabled);
-		tmsStore.set('imanonashiDeleteEnabled', tmsImanonashiDeleteEnabled);
+		tmsStore.set('imanonashiConfirm', tmsImanonashiConfirm);
+		tmsStore.set('imanonashiItself', tmsImanonashiItself);
 
 		const { canceled } = await os.confirm({
 			type: 'info',
