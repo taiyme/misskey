8decf0b3394ec7ff49cf59e5bbef1834e5edf34f
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-15 05:40:24 +0900

    enhance(frontend): モバイル表示の公開範囲ピッカーを見やすくする

--- packages/client/src/components/MkVisibilityPicker.vue
+++ packages/client/src/components/MkVisibilityPicker.vue
@@ -1,42 +1,42 @@
 <template>
-<MkModal ref="modal" :z-priority="'high'" :src="src" @click="modal.close()" @closed="emit('closed')">
-	<div class="gqyayizv _popup">
+<MkModal ref="modal" v-slot="{ type }" :z-priority="'high'" :src="src" @click="modal?.close()" @closed="emit('closed')">
+	<div class="gqyayizv _popup" :class="{ asDrawer: type === 'drawer' }">
 		<button key="public" class="_button" :class="{ active: v === 'public' }" data-index="1" @click="choose('public')">
-			<div><i class="ti ti-world"></i></div>
-			<div>
-				<span>{{ i18n.ts._visibility.public }}</span>
-				<span>{{ i18n.ts._visibility.publicDescription }}</span>
+			<div class="prefix"><i class="ti ti-world"></i></div>
+			<div class="text">
+				<span class="label">{{ i18n.ts._visibility.public }}</span>
+				<span class="description">{{ i18n.ts._visibility.publicDescription }}</span>
 			</div>
 		</button>
 		<button key="home" class="_button" :class="{ active: v === 'home' }" data-index="2" @click="choose('home')">
-			<div><i class="ti ti-home"></i></div>
-			<div>
-				<span>{{ i18n.ts._visibility.home }}</span>
-				<span>{{ i18n.ts._visibility.homeDescription }}</span>
+			<div class="prefix"><i class="ti ti-home"></i></div>
+			<div class="text">
+				<span class="label">{{ i18n.ts._visibility.home }}</span>
+				<span class="description">{{ i18n.ts._visibility.homeDescription }}</span>
 			</div>
 		</button>
 		<button key="followers" class="_button" :class="{ active: v === 'followers' }" data-index="3" @click="choose('followers')">
-			<div><i class="ti ti-lock"></i></div>
-			<div>
-				<span>{{ i18n.ts._visibility.followers }}</span>
-				<span>{{ i18n.ts._visibility.followersDescription }}</span>
+			<div class="prefix"><i class="ti ti-lock"></i></div>
+			<div class="text">
+				<span class="label">{{ i18n.ts._visibility.followers }}</span>
+				<span class="description">{{ i18n.ts._visibility.followersDescription }}</span>
 			</div>
 		</button>
 		<button key="specified" :disabled="localOnly" class="_button" :class="{ active: v === 'specified' }" data-index="4" @click="choose('specified')">
-			<div><i class="ti ti-mail"></i></div>
-			<div>
-				<span>{{ i18n.ts._visibility.specified }}</span>
-				<span>{{ i18n.ts._visibility.specifiedDescription }}</span>
+			<div class="prefix"><i class="ti ti-mail"></i></div>
+			<div class="text">
+				<span class="label">{{ i18n.ts._visibility.specified }}</span>
+				<span class="description">{{ i18n.ts._visibility.specifiedDescription }}</span>
 			</div>
 		</button>
 		<div class="divider"></div>
 		<button key="localOnly" class="_button localOnly" :class="{ active: localOnly }" data-index="5" @click="localOnly = !localOnly">
-			<div><i class="ti ti-world-off"></i></div>
-			<div>
-				<span>{{ i18n.ts._visibility.disableFederation }}</span>
-				<span>{{ i18n.ts._visibility.disableFederationDescription }}</span>
+			<div class="prefix"><i class="ti ti-world-off"></i></div>
+			<div class="text">
+				<span class="label">{{ i18n.ts._visibility.disableFederation }}</span>
+				<span class="description">{{ i18n.ts._visibility.disableFederationDescription }}</span>
 			</div>
-			<div><i :class="localOnly ? 'ti ti-toggle-right' : 'ti ti-toggle-left'"></i></div>
+			<div class="suffix"><i :class="localOnly ? 'ti ti-toggle-right' : 'ti ti-toggle-left'"></i></div>
 		</button>
 	</div>
 </MkModal>
@@ -74,7 +74,7 @@ function choose(visibility: typeof misskey.noteVisibilities[number]): void {
 	v = visibility;
 	emit('changeVisibility', visibility);
 	nextTick(() => {
-		modal.close();
+		modal?.close();
 	});
 }
 </script>
@@ -115,7 +115,7 @@ function choose(visibility: typeof misskey.noteVisibilities[number]): void {
 			background: inherit;
 		}
 
-		> *:nth-child(1) {
+		> .prefix {
 			display: flex;
 			justify-content: center;
 			align-items: center;
@@ -127,23 +127,20 @@ function choose(visibility: typeof misskey.noteVisibilities[number]): void {
 			margin-bottom: auto;
 		}
 
-		> *:nth-child(2) {
+		> .text {
 			flex: 1 1 auto;
-			white-space: nowrap;
-			overflow: hidden;
-			text-overflow: ellipsis;
 
-			> span:first-child {
+			> .label {
 				display: block;
 				font-weight: bold;
 			}
 
-			> span:last-child:not(:first-child) {
+			> .description {
 				opacity: 0.6;
 			}
 		}
 
-		> *:nth-child(3) {
+		> .suffix {
 			display: flex;
 			justify-content: center;
 			align-items: center;
@@ -154,6 +151,14 @@ function choose(visibility: typeof misskey.noteVisibilities[number]): void {
 			margin-top: auto;
 			margin-bottom: auto;
 		}
+	}
+
+	&.asDrawer {
+		padding: 12px 0 max(env(safe-area-inset-bottom, 0px), 12px) 0;
+		width: 100%;
+		border-radius: 24px;
+		border-bottom-right-radius: 0;
+		border-bottom-left-radius: 0;
 	}
 }
 </style>
