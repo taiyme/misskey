6d7df07f20a938ecb479af8b2219661d48c511e7
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-24 00:51:07 +0900

    tweak sub-icon

--- packages/client/src/components/MkNotification.vue
+++ packages/client/src/components/MkNotification.vue
@@ -219,7 +219,7 @@ useTooltip(reactionRef, (showing) => {
 			border-radius: 100%;
 			background: var(--panel);
 			box-shadow: 0 0 0 3px var(--panel);
-			font-size: 12px;
+			font-size: 11px;
 			text-align: center;
 
 			&:empty {
