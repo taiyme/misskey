3b83b524d7ae6cc08f57875bff7e54c7849ff00c
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-25 09:22:38 +0900

    enhance: mid

--- packages/client/src/scripts/tms/parse-mfm-text.ts
+++ packages/client/src/scripts/tms/parse-mfm-text.ts
@@ -1,48 +1,65 @@
 import { VNode, h } from 'vue';
 import MkLinkDlsite from '@/components/MkDlsite.link.vue';
 
-export const REGEX_DLSITE_PID = /[RVB][JE]\d{5,}/g;
-export const DLSITE_PRODUCT_URL = 'https://www.dlsite.com/home/work/=/product_id/';
+export const DLSITE_PID = 'dlsite_pid';
+export const DLSITE_PID_REGEXP = /[RVB][JE]\d{5,}/g;
+export const DLSITE_PID_BASEURL = 'https://www.dlsite.com/home/work/=/product_id/';
+
+export const DLSITE_MID = 'dlsite_mid';
+export const DLSITE_MID_REGEXP = /[RVB][G]\d{5,}/g;
+export const DLSITE_MID_BASEURL = 'https://www.dlsite.com/home/circle/profile/=/maker_id/';
 
 type ParsedMfmText = {
-	type: 'text';
+	type: 'text' | typeof DLSITE_PID | typeof DLSITE_MID;
 	value: string;
-} | {
-	type: 'dlsite_pid';
-	value: string;
-	url: `${typeof DLSITE_PRODUCT_URL}${string}`;
-};
+}
 
 export const parseMfmText = (text: string, parents: string[]): (VNode | string)[] => {
 	if (parents.includes('link') || parents.includes('plain')) return [text];
 
-	const _parse = (_text: string): ParsedMfmText[] => {
-		const matches = _text.match(REGEX_DLSITE_PID);
-		if (!matches) return [{ type: 'text', value: _text }];
+	const _parse = (parsed: ParsedMfmText[], type: ParsedMfmText['type'], regexp: RegExp): ParsedMfmText[] => {
+		return parsed.flatMap(props => {
+			if (props.type !== 'text') return props;
 
-		const result: ParsedMfmText[] = [];
-		let currentIndex = 0;
+			const matches = props.value.match(regexp);
+			if (!matches) return props;
 
-		matches.forEach(match => {
-			const index = text.indexOf(match, currentIndex);
-			if (index > currentIndex) result.push({ type: 'text', value: text.substring(currentIndex, index) });
-			result.push({ type: 'dlsite_pid', value: match, url: `${DLSITE_PRODUCT_URL}${match}` });
-			currentIndex = index + match.length;
-		});
+			const result: ParsedMfmText[] = [];
+			let currentIndex = 0;
 
-		if (currentIndex < text.length) result.push({ type: 'text', value: text.substring(currentIndex) });
-		return result;
+			matches.forEach(match => {
+				const index = props.value.indexOf(match, currentIndex);
+				if (index > currentIndex) result.push({ type: 'text', value: text.substring(currentIndex, index) });
+				result.push({ type, value: match });
+				currentIndex = index + match.length;
+			});
+
+			if (currentIndex < props.value.length) result.push({ type: 'text', value: props.value.substring(currentIndex) });
+			return result;
+		}, Infinity);
 	};
 
-	return _parse(text).map((props) => {
+	let parsed: ParsedMfmText[] = [{ type: 'text', value: text }];
+	parsed = _parse(parsed, 'dlsite_pid', DLSITE_PID_REGEXP);
+	parsed = _parse(parsed, 'dlsite_mid', DLSITE_MID_REGEXP);
+
+	return parsed.map(props => {
 		const { type, value } = props;
 		switch (type) {
 			case 'text': {
 				return value;
 			}
 			case 'dlsite_pid': {
-				const { url } = props;
-				return h(MkLinkDlsite, { key: `${type}:${value}`, url }, value);
+				return h(MkLinkDlsite, {
+					key: value,
+					url: `${DLSITE_PID_BASEURL}${value}`,
+				}, value);
+			}
+			case 'dlsite_mid': {
+				return h(MkLinkDlsite, {
+					key: value,
+					url: `${DLSITE_MID_BASEURL}${value}`,
+				}, value);
 			}
 		}
 	});
