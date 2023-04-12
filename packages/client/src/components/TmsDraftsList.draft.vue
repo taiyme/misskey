3309bac10dd89f36eeb511792092558e331a3a50
taiyme <53635909+taiyme@users.noreply.github.com>
2023-04-13 03:15:27 +0900

    fix

--- packages/client/src/components/TmsDraftsList.draft.vue
+++ packages/client/src/components/TmsDraftsList.draft.vue
@@ -1,6 +1,12 @@
 <template>
 <div :class="$style.root" @click="draftMenu">
-	<div :class="$style.text">{{ props.draft.data.text }}</div>
+	<div :class="$style.labels">
+		<span v-if="props.draft.data.visibility !== 'public'" :class="$style.label">{{ i18n.ts._visibility[props.draft.data.visibility] }}</span>
+		<span v-if="props.draft.data.localOnly" :class="$style.label">{{ i18n.ts._visibility.disableFederation }}</span>
+		<span v-if="props.draft.data.replyId" :class="$style.label">{{ i18n.ts.reply }}</span>
+		<span v-if="props.draft.data.renoteId" :class="$style.label">{{ i18n.ts.quote }}</span>
+	</div>
+	<div :class="[$style.text, { [$style.textEmpty]: !text }]">{{ text || 'Empty' }}</div>
 </div>
 </template>
 
@@ -8,6 +14,7 @@
 import { } from 'vue';
 import * as os from '@/os';
 import { DraftWithId } from '@/scripts/tms/drafts';
+import { i18n } from '@/i18n';
 
 const props = defineProps<{
 	draft: DraftWithId;
@@ -18,6 +25,8 @@ const emit = defineEmits<{
 	(ev: 'deleted', draftId: DraftWithId['id']): void;
 	(ev: 'closed'): void;
 }>();
+
+const text = $ref(`${props.draft.data.cw || ''}\n${props.draft.data.text || ''}`.trim());
 
 const draftMenu = (ev: MouseEvent): void => {
 	const el = ev.currentTarget ?? ev.target;
@@ -45,6 +54,38 @@ const draftMenu = (ev: MouseEvent): void => {
 .root {
 	padding: 16px;
 	background: var(--panel);
+	color: var(--fg);
+	border: solid 1px var(--divider);
+	border-radius: 8px;
 	user-select: none;
+	margin-bottom: var(--margin);
+}
+
+.labels {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 4px;
+	font-size: 0.9em;
+	margin-bottom: 4px;
+}
+
+.label {
+	border: solid 1px var(--divider);
+	border-radius: 999px;
+	margin-right: 4px;
+	padding: 3px 8px;
+}
+
+.text {
+	overflow: hidden;
+	white-space: pre-wrap;
+	overflow-wrap: break-word;
+	display: -webkit-box;
+	-webkit-box-orient: vertical;
+	-webkit-line-clamp: 3;
+}
+
+.textEmpty {
+	opacity: 0.5;
 }
 </style>
