f6678319733285e28f524724fbcaceca4b5d413d
taiyme <53635909+taiyme@users.noreply.github.com>
2023-04-17 08:02:30 +0900

    Revert "test"
    This reverts commit a21fdc605a61305945e60b599853ed940ffad9d6.

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -542,16 +542,10 @@ mainRouter.addListener('same', () => {
 	window.scroll({ top: 0, behavior: 'smooth' });
 });
 
-let disableHistoryHandler = false;
-
 window.addEventListener('popstate', (event) => {
-	if (disableHistoryHandler) return;
-
 	const { historyId } = getHistoryState(event.state);
 
 	if (historyId) {
-		disableHistoryHandler = true;
-
 		if (historyId === '_used_backward_') {
 			history.replaceState(mergeHistoryState({ historyId: '_used_forward_' }), '', location.href);
 			history.forward();
@@ -574,8 +568,6 @@ window.addEventListener('popstate', (event) => {
 				history.back();
 			}
 		}
-
-		disableHistoryHandler = false;
 	}
 
 	mainRouter.replace(location.pathname + location.search + location.hash, event.state?.key, false);
