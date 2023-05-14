e5516691013d1ff65d124bdc2a83555832fb09fa
CaffeinePower <cffnpwr@gmail.com>
2023-05-14 01:28:45 +0000

    update: TmsStatusのパターンマッチにts-patternを使用

--- packages/client/src/components/global/TmsStatus.vue
+++ packages/client/src/components/global/TmsStatus.vue
@@ -14,35 +14,36 @@
 
 <script lang="ts" setup>
 import { computed } from 'vue';
+import { match } from 'ts-pattern';
 import MkButton from '@/components/MkButton.vue';
 import { i18n } from '@/i18n';
 import { defaultStore } from '@/store';
 import customImage from '@/tms/custom-image';
 
-const props = defineProps<{ type?: 'info' | 'notfound' | 'error'; retry?: () => void }>();
+const props = defineProps<{ type: 'info' | 'notfound' | 'error'; retry?: () => void; }>();
 
-const imageUrl = computed(() => {
-	if (props.type === 'info') return customImage.infoImageURL;
-	if (props.type === 'notfound') return customImage.notFoundImageURL;
-	if (props.type === 'error') return customImage.errorImageURL;
-	return null;
-});
+const imageUrl = computed(() => match(props.type)
+	.with('info', () => customImage.infoImageURL)
+	.with('notfound', () => customImage.notFoundImageURL)
+	.with('error', () => customImage.errorImageURL)
+	.exhaustive(),
+);
 </script>
 
 <style lang="scss" module>
 .root {
-  display: flex;
-  flex-direction: column;
-  gap: var(--margin);
-  padding: 32px;
-  text-align: center;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	gap: var(--margin);
+	padding: 32px;
+	text-align: center;
+	align-items: center;
 }
 
 .image {
-  display: inline-block;
-  height: 128px;
-  border-radius: 16px;
-  vertical-align: bottom;
+	display: inline-block;
+	height: 128px;
+	border-radius: 16px;
+	vertical-align: bottom;
 }
 </style>
