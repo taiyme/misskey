f3ad9fcc9819a9bb61cbc437275ec79fe8aa74e3
taiyme <53635909+taiyme@users.noreply.github.com>
2023-10-12 16:44:00 +0900

    Update Copyright (packages/frontend)

--- packages/frontend/src/components/page/page.block.vue
+++ packages/frontend/src/components/page/page.block.vue
@@ -1,5 +1,6 @@
 <!--
 SPDX-FileCopyrightText: syuilo and other misskey contributors
+SPDX-FileCopyrightText: Copyright © 2023 taiy https://github.com/taiyme
 SPDX-License-Identifier: AGPL-3.0-only
 -->
 
