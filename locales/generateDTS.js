70ac55c61cf9522f2dc364ff851748fe5f2fccb3
taiyme <53635909+taiyme@users.noreply.github.com>
2023-10-13 14:29:17 +0900

    Locale型でreadonlyを使用する

--- locales/generateDTS.js
+++ locales/generateDTS.js
@@ -8,15 +8,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 function createMembers(record) {
-	return Object.entries(record)
-		.map(([k, v]) => ts.factory.createPropertySignature(
-			undefined,
+	return Object.entries(record).map(([k, v]) => {
+		return ts.factory.createPropertySignature(
+			[ts.factory.createModifier(ts.SyntaxKind.ReadonlyKeyword)],
 			ts.factory.createStringLiteral(k),
 			undefined,
 			typeof v === 'string'
 				? ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword)
 				: ts.factory.createTypeLiteralNode(createMembers(v)),
-		));
+		);
+	});
 }
 
 export default function generateDTS() {
