6e6ef41bbf569c7a331742f3df395043e2b762d3
taiyme <53635909+taiyme@users.noreply.github.com>
2023-10-14 19:51:23 +0900

    chore: ユニオン型のパイプ記号は先頭にあった方が良い

--- packages/frontend/src/local-storage.ts
+++ packages/frontend/src/local-storage.ts
@@ -5,38 +5,38 @@
  */
 
 type Keys =
-	'v' |
-	'lastVersion' |
-	'instance' |
-	'account' |
-	'accounts' |
-	'latestDonationInfoShownAt' |
-	'neverShowDonationInfo' |
-	'neverShowLocalOnlyInfo' |
-	'lastUsed' |
-	'lang' |
-	'drafts' |
-	'hashtags' |
-	'wallpaper' |
-	'theme' |
-	'colorScheme' |
-	'useSystemFont' |
-	'fontSize' |
-	'ui' |
-	'ui_temp' |
-	'locale' |
-	'localeVersion' |
-	'theme' |
-	'customCss' |
-	'message_drafts' |
-	'scratchpad' |
-	'debug' |
-	`miux:${string}` |
-	`ui:folder:${string}` |
-	`themes:${string}` |
-	`aiscript:${string}` |
-	'lastEmojisFetchedAt' | // DEPRECATED, stored in indexeddb (13.9.0~)
-	'emojis' // DEPRECATED, stored in indexeddb (13.9.0~);
+	| 'v'
+	| 'lastVersion'
+	| 'instance'
+	| 'account'
+	| 'accounts'
+	| 'latestDonationInfoShownAt'
+	| 'neverShowDonationInfo'
+	| 'neverShowLocalOnlyInfo'
+	| 'lastUsed'
+	| 'lang'
+	| 'drafts'
+	| 'hashtags'
+	| 'wallpaper'
+	| 'theme'
+	| 'colorScheme'
+	| 'useSystemFont'
+	| 'fontSize'
+	| 'ui'
+	| 'ui_temp'
+	| 'locale'
+	| 'localeVersion'
+	| 'theme'
+	| 'customCss'
+	| 'message_drafts'
+	| 'scratchpad'
+	| 'debug'
+	| `miux:${string}`
+	| `ui:folder:${string}`
+	| `themes:${string}`
+	| `aiscript:${string}`
+	| 'lastEmojisFetchedAt' // DEPRECATED, stored in indexeddb (13.9.0~)
+	| 'emojis' // DEPRECATED, stored in indexeddb (13.9.0~);
 
 export const miLocalStorage = {
 	getItem: (key: Keys): string | null => window.localStorage.getItem(key),
