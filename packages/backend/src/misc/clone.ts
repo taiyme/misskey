4f7fbb69792e0e28f767c6b0e6fb7aae99cbe9ff
taiyme <53635909+taiyme@users.noreply.github.com>
2023-11-05 20:27:07 +0900

    fix(frontend/backend): deepCloneの戻り値にundefinedを含まないように
    resolve #98

--- packages/backend/src/misc/clone.ts
+++ packages/backend/src/misc/clone.ts
@@ -7,7 +7,7 @@
 // structredCloneが遅いため
 // SEE: http://var.blog.jp/archives/86038606.html
 
-type Cloneable = string | number | boolean | null | { [key: string]: Cloneable } | Cloneable[];
+type Cloneable = string | number | boolean | null | { [key: string]: Cloneable | undefined } | Cloneable[];
 
 export function deepClone<T extends Cloneable>(x: T): T {
 	if (typeof x === 'object') {
@@ -15,6 +15,7 @@ export function deepClone<T extends Cloneable>(x: T): T {
 		if (Array.isArray(x)) return x.map(deepClone) as T;
 		const obj = {} as Record<string, Cloneable>;
 		for (const [k, v] of Object.entries(x)) {
+			if (v === undefined) continue;
 			obj[k] = deepClone(v);
 		}
 		return obj as T;
--- packages/frontend/src/scripts/clone.ts
+++ packages/frontend/src/scripts/clone.ts
@@ -9,7 +9,7 @@
 // あと、Vue RefをIndexedDBに保存しようとしてstructredCloneを使ったらエラーになった
 // https://github.com/misskey-dev/misskey/pull/8098#issuecomment-1114144045
 
-type Cloneable = string | number | boolean | null | { [key: string]: Cloneable } | Cloneable[];
+type Cloneable = string | number | boolean | null | { [key: string]: Cloneable | undefined } | Cloneable[];
 
 export function deepClone<T extends Cloneable>(x: T): T {
 	if (typeof x === 'object') {
@@ -17,6 +17,7 @@ export function deepClone<T extends Cloneable>(x: T): T {
 		if (Array.isArray(x)) return x.map(deepClone) as T;
 		const obj = {} as Record<string, Cloneable>;
 		for (const [k, v] of Object.entries(x)) {
+			if (v === undefined) continue;
 			obj[k] = deepClone(v);
 		}
 		return obj as T;
