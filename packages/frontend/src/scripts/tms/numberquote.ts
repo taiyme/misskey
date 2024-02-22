7b61fb47427ed67dd8574dfa6564c47c85d2fdea
taiyme <53635909+taiyme@users.noreply.github.com>
2024-02-22 19:09:55 +0900

    recommendSuffixSpace, recommendSuffixNewLine

--- packages/frontend/src/scripts/tms/numberquote.ts
+++ packages/frontend/src/scripts/tms/numberquote.ts
@@ -4,6 +4,7 @@
  */
 
 import * as Misskey from 'misskey-js';
+import * as mfm from 'mfm-js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
 import { NoteEntityOrId, NoteParameters, toParameters } from '@/scripts/tms/to-parameters.js';
 
@@ -11,28 +12,6 @@ export const numberquote = async (noteEntityOrId: NoteEntityOrId, fromId?: strin
 	const { parameters, me } = await toParameters(noteEntityOrId, fromId);
 	parameters.text = incrText(parameters.text);
 	return misskeyApi('notes/create', parameters, me.token);
-};
-
-const extractNqTokens = (text: NoteParameters['text']): {
-	readonly str: string;
-	readonly num: string;
-} => {
-	if (text == null || text === '') {
-		return { str: '', num: '' } as const;
-	}
-	if (text.endsWith('</center>')) {
-		const str = `${text}\n`;
-		return { str, num: '' } as const;
-	}
-	if (text.endsWith(':')) {
-		const ZWSP = '\u200b';
-		const str = `${text}${ZWSP}`;
-		return { str, num: '' } as const;
-	}
-	const result = text.split(/(\-?\d+)$/);
-	const str = result.at(0) ?? '';
-	const num = result.at(1) ?? '';
-	return { str, num } as const;
 };
 
 const incrText = (text: NoteParameters['text']): NoteParameters['text'] => {
@@ -45,4 +24,39 @@ const decrText = (text: NoteParameters['text']): NoteParameters['text'] => {
 	const token = extractNqTokens(text);
 	if (token.num === '') return `${token.str}0`;
 	return `${token.str}${(BigInt(token.num) - 1n).toString(10)}`;
+};
+
+const extractNqTokens = (text: NoteParameters['text']): {
+	readonly str: string;
+	readonly num: string;
+} => {
+	if (text == null || text === '') {
+		return { str: '', num: '' } as const;
+	}
+	const ast = mfm.parse(text);
+	const lastNode = ast.at(-1) ?? null;
+	if (recommendSuffixSpace(lastNode)) {
+		const str = `${text} `;
+		return { str, num: '' } as const;
+	}
+	if (recommendSuffixNewLine(lastNode)) {
+		const str = `${text}\n`;
+		return { str, num: '' } as const;
+	}
+	const result = text.split(/(\-?\d+)$/);
+	const str = result.at(0) ?? '';
+	const num = result.at(1) ?? '';
+	return { str, num } as const;
+};
+
+const recommendSuffixSpace = (node: mfm.MfmNode | null): boolean => {
+	if (node == null) return false;
+	if (node.type === 'url' && !node.props.brackets) return true;
+	if (node.type === 'text' && node.props.text.endsWith('-')) return true;
+	return ['mention', 'hashtag', 'emojiCode'].includes(node.type);
+};
+
+const recommendSuffixNewLine = (node: mfm.MfmNode | null): boolean => {
+	if (node == null) return false;
+	return ['quote', 'search', 'blockCode', 'mathBlock', 'center'].includes(node.type);
 };
