33f402c2688d7163a5df249adeb2934fc1491142
taiyme <53635909+taiyme@users.noreply.github.com>
2024-03-08 15:58:55 +0900

    feat(tms/mfm): KaTeX記法のサポート

---
+++ packages/frontend/src/components/TmsFormula.vue
@@ -0,0 +1,19 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+<XFormula :formula="props.formula" :block="props.block"/>
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps<{
+	formula: string;
+	block: boolean;
+}>();
+
+const XFormula = defineAsyncComponent(() => import('@/components/TmsFormula.core.vue'));
+</script>
