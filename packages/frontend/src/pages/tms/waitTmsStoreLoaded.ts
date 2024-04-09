3c5b06f9a80d9103f3eb30cacfe199c69c1b2997
CaffeinePower <cffnpwr@gmail.com>
2024-04-09 17:04:42 +0000

    ライセンス表記を追記

--- packages/frontend/src/pages/tms/waitTmsStoreLoaded.ts
+++ packages/frontend/src/pages/tms/waitTmsStoreLoaded.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and other misskey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { tmsFlaskStore } from '@/tms/flask-store.js';
 import { tmsStore } from '@/tms/store.js';
 
