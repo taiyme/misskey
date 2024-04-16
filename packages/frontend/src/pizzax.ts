0eede7a0f7318fc5cf0862d4f42a53a2e0c792af
taiyme <53635909+taiyme@users.noreply.github.com>
2024-04-16 17:13:06 +0900

    fix(pizzax): makeGetterSetter の引数の型を修正

--- packages/frontend/src/pizzax.ts
+++ packages/frontend/src/pizzax.ts
@@ -240,7 +240,7 @@ export class Storage<T extends StateDef> {
 	 * 特定のキーの、簡易的なgetter/setterを作ります
 	 * 主にvue上で設定コントロールのmodelとして使う用
 	 */
-	public makeGetterSetter<K extends keyof T>(key: K, getter?: (v: T[K]) => unknown, setter?: (v: unknown) => T[K]): {
+	public makeGetterSetter<K extends keyof T>(key: K, getter?: (v: T[K]['default']) => unknown, setter?: (v: unknown) => T[K]['default']): {
 		get: () => T[K]['default'];
 		set: (value: T[K]['default']) => void;
 	} {
