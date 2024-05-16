7202ddfda83619a5ccdfe1a42e7182ab5f7163b6
taiyme <53635909+taiyme@users.noreply.github.com>
2024-05-16 20:04:05 +0900

    Revert(backend): 2b21c1936212b6e1288d545b71544888e84ce8ab
    This reverts commit 2b21c1936212b6e1288d545b71544888e84ce8ab.
    
    * backend以外, typescript, nodeは除外

--- packages/backend/src/server/api/endpoints/i/2fa/key-done.ts
+++ packages/backend/src/server/api/endpoints/i/2fa/key-done.ts
@@ -96,10 +96,10 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			}
 
 			const keyInfo = await this.webAuthnService.verifyRegistration(me.id, ps.credential);
-			const keyId = keyInfo.credentialID;
 
+			const credentialId = Buffer.from(keyInfo.credentialID).toString('base64url');
 			await this.userSecurityKeysRepository.insert({
-				id: keyId,
+				id: credentialId,
 				userId: me.id,
 				name: ps.name,
 				publicKey: Buffer.from(keyInfo.credentialPublicKey).toString('base64url'),
@@ -116,7 +116,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			}));
 
 			return {
-				id: keyId,
+				id: credentialId,
 				name: ps.name,
 			};
 		});
