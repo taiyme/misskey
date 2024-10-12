72b202d7253eafa337b12744231961c946e8a630
taiyme <53635909+taiyme@users.noreply.github.com>
2024-10-12 15:23:52 +0900

    Revert "hotfix?(frontend): getBgColorの判定式を軽量化"
    This reverts commit 06155bfe0222c38b03b9cb9d7603c89e3d042a34.

--- packages/frontend/src/scripts/tms/get-bg-color.ts
+++ packages/frontend/src/scripts/tms/get-bg-color.ts
@@ -3,12 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import tinycolor from 'tinycolor2';
+
 export const getBgColor = (elem?: Element | null | undefined): string | null => {
 	if (elem == null) return null;
 
 	const { backgroundColor: bg } = window.getComputedStyle(elem);
 
-	if (bg && !['rgba(0, 0, 0, 0)', 'rgba(0,0,0,0)', 'transparent'].includes(bg)) {
+	if (bg && tinycolor(bg).getAlpha() !== 0) {
 		return bg;
 	}
 
